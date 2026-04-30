163ba2094b0cfd7e8cdcddcc4960e56ca3514ac1
wannabedigital <digitaldxmmy@gmail.com>
2026-04-30 15:29:41 +0300

    refactor: took Contact Form separately

---
+++ src/components/Loading.tsx
@@ -0,0 +1,29 @@
+import { colors } from '@/src/constants/colors';
+
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+interface LoadingProps {
+  text?: string;
+}
+
+export const Loading = ({ text }: LoadingProps) => {
+  return (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size='large' color={colors.primary} />
+      <Text style={styles.loadingText}>{text || 'Загрузка...'}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.background,
+  },
+  loadingText: {
+    marginTop: 10,
+    color: colors.textSecondary,
+  },
+});
